0ff3eca7f13efb25b9f3719ab1001a731fe2551a
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-04-26 00:49:06 +0100

    Refactor by removing {@link ListView} from fragment_gates.xml into a layout file site_listview.xml for all Fragments to utilise.

--- app/src/main/java/com/blogspot/thengnet/kanotourist/GatesFragment.java
+++ app/src/main/java/com/blogspot/thengnet/kanotourist/GatesFragment.java
@@ -44,7 +44,7 @@ public class GatesFragment extends Fragment {
     public View onCreateView (LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gates, container, false);
+        return inflater.inflate(R.layout.site_listview, container, false);
     }
 
     @Override
