87cba6461f746b94f624510a33f33c848237eb8a
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-04-26 00:00:53 +0100

    Rename SiteAdapter.java to Site.java

--- app/src/main/java/com/blogspot/thengnet/kanotourist/Site.java
+++ app/src/main/java/com/blogspot/thengnet/kanotourist/Site.java
@@ -1,21 +1,21 @@
 package com.blogspot.thengnet.kanotourist;
 
-public class SiteAdapter {
+public class Site {
 
     private String mSiteName;
     private int mImageResourceId;
 
-    public SiteAdapter (String mSiteName, int mImageResourceId) {
+    public Site (String mSiteName, int mImageResourceId) {
         this.mSiteName = mSiteName;
         this.mImageResourceId = mImageResourceId;
     }
 
-    public SiteAdapter setSiteName (String mSiteName) {
+    public Site setSiteName (String mSiteName) {
         this.mSiteName = mSiteName;
         return this;
     }
 
-    public SiteAdapter setImageResourceId (int mImageResourceId) {
+    public Site setImageResourceId (int mImageResourceId) {
         this.mImageResourceId = mImageResourceId;
         return this;
     }
