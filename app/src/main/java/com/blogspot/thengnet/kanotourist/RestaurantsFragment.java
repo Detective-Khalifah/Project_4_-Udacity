d1b29c7a64bb0843e92ff038aceee6b21f0f0113
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-04-25 23:58:09 +0100

    Add RestaurantsFragment {@link Fragment}

---
+++ app/src/main/java/com/blogspot/thengnet/kanotourist/RestaurantsFragment.java
@@ -0,0 +1,42 @@
+package com.blogspot.thengnet.kanotourist;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RestaurantsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RestaurantsFragment extends Fragment {
+
+    public RestaurantsFragment () {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment RestaurantsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RestaurantsFragment newInstance () {
+        return new RestaurantsFragment();
+    }
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_restaurants, container, false);
+    }
+}
