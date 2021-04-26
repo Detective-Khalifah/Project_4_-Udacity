a29750b3c199e7ee8ff6089f463e3a3db23b9d9f
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-04-26 04:59:13 +0100

    Set up MarketsFragment

--- app/src/main/java/com/blogspot/thengnet/kanotourist/MarketsFragment.java
+++ app/src/main/java/com/blogspot/thengnet/kanotourist/MarketsFragment.java
@@ -1,10 +1,17 @@
 package com.blogspot.thengnet.kanotourist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -12,6 +19,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MarketsFragment extends Fragment {
+
+    private ArrayList<Site> mMarketsList;
 
     public MarketsFragment () {
         // Required empty public constructor
@@ -30,12 +39,44 @@ public class MarketsFragment extends Fragment {
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mMarketsList = new ArrayList<>();
+        mMarketsList.add(new Site("'Yankaba"));
+        mMarketsList.add(new Site("Bachirawa"));
+        mMarketsList.add(new Site("Dawanau"));
+        mMarketsList.add(new Site("Galadima"));
+        mMarketsList.add(new Site("Ƙofar Ruwa"));
+        mMarketsList.add(new Site("Kurmi"));
+        mMarketsList.add(new Site("Rimi"));
+        mMarketsList.add(new Site("Sabon Gari"));
+        mMarketsList.add(new Site("Ujile"));
+        mMarketsList.add(new Site("Wafa"));
     }
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_markets, container, false);
+        return inflater.inflate(R.layout.site_listview, container, false);
+    }
+
+    @Override
+    public void onViewCreated (View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final ListView listView = view.findViewById(R.id.list_sites);
+        listView.setAdapter(new SiteAdapter(getContext(), mMarketsList));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                Site theSite = mMarketsList.get(position);
+                Intent immerse = new Intent(getActivity(), ImmersiveViewerActivity.class);
+                immerse.putExtra("SUMMARY", theSite.getSiteSummary()); // always available
+
+                if (theSite.hasImage())
+                    immerse.putExtra("IMG", theSite.getImageResourceId());
+                if (theSite.hasLink())
+                    immerse.putExtra("LINK", mMarketsList.get(position).getSiteLink());
+                startActivity(immerse);
+            }
+        });
     }
 }
