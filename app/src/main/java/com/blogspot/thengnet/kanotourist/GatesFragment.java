7da3376558e64c9db543f7870072e1889b6e274e
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-04-26 03:31:21 +0100

    Set up Gates Edit site.xml -change visibility of link & image to invisible, as they're not always available and gap/spacing looks good. -change size of image to 192x192

--- app/src/main/java/com/blogspot/thengnet/kanotourist/GatesFragment.java
+++ app/src/main/java/com/blogspot/thengnet/kanotourist/GatesFragment.java
@@ -1,11 +1,14 @@
 package com.blogspot.thengnet.kanotourist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -35,11 +38,37 @@ public class GatesFragment extends Fragment {
     @Override
     public void onCreate (Bundle savedInstanceState) {
         mGateSites = new ArrayList<>();
-        mGateSites.add(new Site("Kofar Kansakali", R.drawable.kofar_kansakali_1,
-                getString(R.string.kofar_kansakali_link)));
-        mGateSites.add(new Site("Kofar Nassarawa", R.drawable.kofar_nasarawa_2));
-        mGateSites.add(new Site("Kofar Rumfa", R.drawable.kofar_rumfa_or_fada_3));
-
+        mGateSites.add(new Site(getString(R.string.gate_1), R.drawable.kofar_kansakali_1,
+                getString(R.string.gate_1)));
+        mGateSites.add(new Site(getString(R.string.gate_2), getString(R.string.gate_2_link)));
+        mGateSites.add(new Site(getString(R.string.gate_3), R.drawable.kofar_dawanau_3,
+                getString(R.string.gate_3_link)));
+        mGateSites.add(new Site(getString(R.string.gate_4), R.drawable.kofar_ruwa_4,
+                getString(R.string.gate_4_link)));
+        mGateSites.add(new Site(getString(R.string.gate_5), R.drawable.kofar_mazugal_5,
+                getString(R.string.gate_5_link)));
+        mGateSites.add(new Site(getString(R.string.gate_6), R.drawable.kofar_wambai_6,
+                getString(R.string.gate_6_link)));
+        mGateSites.add(new Site(getString(R.string.gate_7), R.drawable.kofar_mata_7,
+                getString(R.string.gate_7_link)));
+        mGateSites.add(new Site(getString(R.string.gate_8), R.drawable.kofar_dan_agundi_8_partially_collapsed,
+                getString(R.string.gate_8_link)));
+        mGateSites.add(new Site(getString(R.string.gate_9), R.drawable.kofar_nasarawa_9,
+                getString(R.string.gate_9_link)));
+        mGateSites.add(new Site(getString(R.string.gate_10), R.drawable.kofar_naisa_10,
+                getString(R.string.gate_10_link)));
+        mGateSites.add(new Site(getString(R.string.gate_11), R.drawable.kofar_gadon_kaya_11,
+                getString(R.string.gate_11_link)));
+        mGateSites.add(new Site(getString(R.string.gate_12), R.drawable.kanokdukawuya_12,
+                getString(R.string.gate_12_link)));
+        mGateSites.add(new Site(getString(R.string.gate_13), R.drawable.kanokkabuga_13,
+                getString(R.string.gate_13_link)));
+        mGateSites.add(new Site(getString(R.string.gate_14), R.drawable.kanokfamfo_14,
+                getString(R.string.gate_14_link)));
+        mGateSites.add(new Site(getString(R.string.gate_15), R.drawable.sabuwar_kofa_15,
+                getString(R.string.gate_15_link)));
+        mGateSites.add(new Site(getString(R.string.gate_16), getString(R.string.gate_16_link)));
+        mGateSites.add(new Site(getString(R.string.gate_17), R.drawable.kofar_fada_17));
 
         super.onCreate(savedInstanceState);
     }
@@ -54,6 +83,18 @@ public class GatesFragment extends Fragment {
     @Override
     public void onViewCreated (View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextView txtGatesIntro = (TextView) view.findViewById(R.id.location_intro);
+        txtGatesIntro.setText(getString(R.string.gates_intro));
+
+        final TextView txtGatesInfo = (TextView) view.findViewById(R.id.location_info);
+        txtGatesInfo.setText(getString(R.string.gates_further_read));
+        txtGatesInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.gates_link))));
+            }
+        });
 
         ListView listView = view.findViewById(R.id.list_gates);
         listView.setAdapter(new SiteAdapter(getContext(), mGateSites));
